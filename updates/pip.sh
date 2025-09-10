7fdc3e80f57a7352acac4e80c322e0a91f02ae06
pydatk <12197575+pydatk@users.noreply.github.com>
2025-09-11 10:50:51 +1200

    removed pip/safety (safety not working)

--- updates/pip.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-# Install pip (system-wide)
-
-sudo apt install python3-pip
--- updates/safety.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-# Install safety CLI
-# https://docs.safetycli.com/safety-docs/safety-cli/introduction-to-safety-cli-vulnerability-scanning
-
-pip install safety
